97fc0612249936e055f192aa0a09347043d9e1fe
Rajat Sahu <sahurjt@gmail.com>
2019-02-09 23:52:58 +0530

    modify json helper

--- apref/src/main/java/com/sahurjt/apref/JsonHelper.java
+++ apref/src/main/java/com/sahurjt/apref/JsonHelper.java
@@ -1,17 +1,33 @@
 package com.sahurjt.apref;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
 import com.google.gson.Gson;
 
 final class JsonHelper {
 
+    @Nullable
     public static <T> T fromJsonString(String jsonString, Class<T> toModel) {
         Gson gson = new Gson();
-        return gson.fromJson(jsonString, toModel);
+        try {
+            return gson.fromJson(jsonString, toModel);
+        } catch (Exception e) {
+            Log.d("library-pref",
+                    String.format("Error in mapping JsonString:%s with model:%s", jsonString, toModel));
+            return null;
+        }
     }
 
 
-    public static <T> void toJsonString(String jsonString, Class<T> fromModel) {
+    public static <T> String toJsonString(T object, Class<T> fromModel) {
         Gson gson = new Gson();
-        gson.toJson(jsonString, fromModel);
+
+        try {
+            return gson.toJson(object, fromModel);
+        } catch (Exception e) {
+            Log.d("library-pref",
+                    String.format("Error in creating json of :%s", fromModel));
+            return "";
+        }
     }
 }
